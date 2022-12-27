bbecf3cd00615d4144600f1ba4511892ae21bf5e
DALMAZIR Agathe <dalmagathe@gmail.com>
2022-12-27 18:47:04 +0100

    Open Recipes list activity.

--- app/src/main/java/com/example/recipefoodslist/MainActivity.java
+++ app/src/main/java/com/example/recipefoodslist/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.recipefoodslist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button OpenRecipeActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OpenRecipeActivity = (Button) findViewById(R.id.RecipeBtn);
+        OpenRecipeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenRecipeActivityFct();
+            }
+        });
+    }
+
+    public void OpenRecipeActivityFct(){
+        Intent intent = new Intent(this, AddRecipe.class);
+        startActivity(intent);
     }
 }
