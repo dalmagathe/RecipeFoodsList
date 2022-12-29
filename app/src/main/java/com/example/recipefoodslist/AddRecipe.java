4e3a920c101d6af5cbb93ad51bae497b89f3feb8
DALMAZIR Agathe <dalmagathe@gmail.com>
2022-12-29 17:09:07 +0100

    Adding ingredients in the list.

--- app/src/main/java/com/example/recipefoodslist/AddRecipe.java
+++ app/src/main/java/com/example/recipefoodslist/AddRecipe.java
@@ -3,44 +3,66 @@ package com.example.recipefoodslist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class AddRecipe extends AppCompatActivity {
 
-    private String[] Ingredient = new String[]{"Pizza", "Burger", "Pasta", "blablabla", "Rice","Sandwich","Chips","bla", "blabla", "test", "Pizza", "Burger", "Pasta", "blablabla"};
-    private int[] Qty = new int[]{3, 4, 2, 1, 5,8,20,50,78,20,20,50,78,20};
-    private String[] Unit = new String[]{"Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test"};
-    private ArrayList<Ingredient> ingredientArrayList;
-    private ListView listView;
+    private String[] Ingredient = new String[]{"Tomate", ""};
+    private int[] Qty = new int[]{500, 0};
+    private String[] Unit = new String[]{"gr", ""};
+    private ArrayList<Ingredient> ingredientArrayList = new ArrayList<Ingredient>(); //arrayList
+    private ListView listView; //lv
+    private IngredientAdapter ingredientAdapter;
+    private int ingredientNB = 0;
+
+    EditText et;
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
 
+        et = (EditText) findViewById(R.id.ingredientName);
+        bt = (Button) findViewById(R.id.addIngredientBtn);
         listView = findViewById(R.id.listView);
 
-        ingredientArrayList = populateList();
-
-        IngredientAdapter ingredientAdapter = new IngredientAdapter(this,ingredientArrayList);
+        //arrayList
+        populateList(ingredientArrayList);
+        //adapter
+        ingredientAdapter = new IngredientAdapter(AddRecipe.this,ingredientArrayList);
         listView.setAdapter(ingredientAdapter);
+
+        onBtnClick();
 
     }
 
-    private ArrayList<Ingredient> populateList(){
+    public void onBtnClick(){
+        ++ingredientNB;
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Ingredient[1] = et.getText().toString();
+                Qty[1] = 3;
+                Unit[1] = "l";
+                populateList(ingredientArrayList);
+                ingredientAdapter.notifyDataSetChanged();
+            }
+        });
+    }
 
-        ArrayList<Ingredient> list = new ArrayList<>();
-
-        for(int i = 0; i < 14; i++){
-            Ingredient ingredientModel = new Ingredient();
-            ingredientModel.setIngredient(Ingredient[i]);
-            ingredientModel.setQty(Qty[i]);
-            ingredientModel.setUnit(Unit[i]);
-            list.add(ingredientModel);
-        }
-
-        return list;
+    private void populateList(ArrayList<Ingredient> ingredientArrayList){
+        Ingredient ingredientModel = new Ingredient();
+        ingredientModel.setIngredient(Ingredient[ingredientNB]);
+        ingredientModel.setQty(Qty[ingredientNB]);
+        ingredientModel.setUnit(Unit[ingredientNB]);
+        ingredientArrayList.add(ingredientModel);
     }
 }
