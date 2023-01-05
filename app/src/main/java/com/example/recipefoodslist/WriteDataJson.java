e41354b99c18b23668e96f13520565ffa371f761
DALMAZIR Agathe <dalmagathe@gmail.com>
2023-01-05 19:15:14 +0100

    Adding value in JSON.

--- app/src/main/java/com/example/recipefoodslist/WriteDataJson.java
+++ app/src/main/java/com/example/recipefoodslist/WriteDataJson.java
@@ -1,6 +1,9 @@
 package com.example.recipefoodslist;
 
+import static java.lang.System.out;
+
 import android.content.Context;
+import android.util.JsonWriter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,18 +12,33 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WriteDataJson {
 
     public static void main(String path, String ingredient) throws IOException, JSONException {
-        JSONObject obj = new JSONObject();
 
-        obj.put("Name", ingredient);
+        JSONArray testArray = new JSONArray();
+
+        JSONObject objNewIngredient = new JSONObject();
+        objNewIngredient.put("Name", "Tomate");
+        objNewIngredient.put("Ingredient", "2");
+        objNewIngredient.put("Unit", "unit");
+        testArray.put(objNewIngredient);
+
+        JSONObject objAllIngredient = new JSONObject();
+        objAllIngredient.put("Ingredients", testArray);
+
+        JSONObject objAllRecipe = new JSONObject();
+        objAllRecipe.put("PateCarbo", objAllIngredient);
 
         try{
             File f = new File(path + "/newTestFile.json");
-            FileWriter file = new FileWriter(f.getAbsoluteFile());
-            file.write(obj.toString());
+            FileWriter file = new FileWriter(f.getAbsoluteFile(), true);
+            file.write(objAllRecipe.toString(2));
             file.flush();
             file.close();
         }
@@ -28,8 +46,7 @@ public class WriteDataJson {
             e.printStackTrace();
         }
 
-
-        System.out.println("File created");
+        out.println("File created");
 
     }
 }
